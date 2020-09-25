286a776416e49f8b1d4eb569baacf23a707d741e
moumita-das-7019 <moumita.das7019@gmail.com>
2020-09-25 16:38:07 +0000

    Ticker and SaveData scripts added

---
+++ scripts/ticker.py
@@ -0,0 +1,17 @@
+#Script to get all ticker data
+#Run frequency: 1st of each month
+#Run time: 3:00 am IST
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import requests
+import json
+
+#Gathering data from API
+response = requests.get('http://api.marketstack.com/v1/tickers?access_key=7a247f5ee1c585714c83b74587866efb&exchange=XNSE')
+jsonResponse = response.json()
+#Making panda dataframe
+
+df = pd.DataFrame(jsonResponse['data']) 
+df = df[['name', 'symbol']]
+df.sort_values(by='name', ascending=1, inplace=True)
+df.to_csv('/home/moumita_das2820/StockPredict/data/ticker.csv',index=False)
+
--- test_StockPredict.py
+++ test_StockPredict.py
@@ -1,21 +1,18 @@
 import streamlit as st
 import pandas as pd
+import os
 from PIL import Image
 # Creating the Titles and Image
 st.title("Indian Stock Market Prediction")
 st.header("Finding a probable stock price using Machine Learning")
-st.write("Please choose a stock and date to get your stock price")
+st.write("Please choose a stock and date to get your stock price:")
 image = Image.open('StockMarket.jpg')
 st.image(image, use_column_width=True)
 st.write("Image Courtesy: https://www.clickastro.com/ogimages/indian-stock-market-predictions.jpg")
-df = pd.DataFrame({
-'Stock_Symbol': ["MARUTI.XNSE", "TCS.XNSE"],
-'Stock_Name':  ["MARUTI", "TCS"]
-})
-
+df = pd.read_csv("/home/moumita_das2820/StockPredict/data/ticker.csv")
+"The Stock price prediction available at our website for these Indain stocks:"
 df
-
-option = st.selectbox('Which Stock do you like best?',df['Stock_Symbol'])
+option = st.selectbox('Which Stock do you like best?',df['symbol'])
 'You selected: ', option
 
 
@@ -32,5 +29,5 @@ if UserDate in imaginary_stock:
         st.write("Predicted stock price for", UserDate, " is ", stockValue)
 
 st.write("""
-Please note: Stock market is unpredictable, and these data is only for reference. Please use your decision making when looking to invest
+Please note: Stock market is unpredictable, and these data is only for reference. Please use your decision making when looking to invest.
 """)
